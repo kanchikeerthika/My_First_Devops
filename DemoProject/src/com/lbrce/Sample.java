2f8fc91f103479e018da1eb602c9dd8e6557a4aa
prasannakanala <125422235+prasannakanala@users.noreply.github.com>
2024-08-22 10:42:46 +0530

    Update Sample.java

--- DemoProject/src/com/lbrce/Sample.java
+++ DemoProject/src/com/lbrce/Sample.java
@@ -3,6 +3,11 @@ package com.lbrce;
 public class Sample {
 	public static void main(String args[]) {
 		System.out.println("Hello World");
+		
 	}
 
 }
+public static class Sample1
+	{
+		System.out.println("Hello Keerthika");
+	}
